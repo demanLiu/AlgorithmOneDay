66788dfcb8a3fd05b0977e2365ddaed4b3a92de8
liuhuan <qq384012351@gmail.com>
2018-05-30 00:29:46 +0800

    finish bigNumber plus

--- bigNumber.c
+++ bigNumber.c
@@ -2,19 +2,52 @@
 #include "string.h"
 #include "stdlib.h"
 
-
-char* multi(char* s,int n){
-    int bit;
+#define BIT_MAX 1000
+#define c_for_each(begin, end, array,type) \
+                   for(int i=(begin); i<(end); ++i) printf(type,array[i]);
+char* multi(char* s){
+    int bit,pointBit,j,tempLen,result[BIT_MAX],improve;
+    char resultStr[BIT_MAX];
     int len =strlen(s);
+    int splitNum[len],count[len];
     char temp[1];
     for(int i=0;i<len;i++){
-        temp[0] = s[i];
-        bit = atoi(temp);
-        // if(s[i] =='.'){
-        //     continue;
-        // }
-        printf("%d\n",bit);
+        if(s[i] =='.'){
+            j = len-i-1;
+            while(j--){
+                pointBit *= 10;
+            }
+            continue;
+        }
+        temp[0] =s[i];
+        splitNum[tempLen++] = atoi(temp);
     }
+    c_for_each(0,bit,splitNum,"%d");
+    for(int i=tempLen;i>0;i--){
+        bit = tempLen-i;
+        for(int m=tempLen;m>0;m--){
+           result[bit++] += splitNum[i-1]*splitNum[m-1];
+        }
+    }
+    c_for_each(0,bit,result,"%d");
+    printf("resultstr:\n");
+    printf("%d\n",bit);
+    improve = 0;
+    for(int i=bit;i>0;i--){
+        result[i-1] += improve;
+        improve = result[i-1]/10;
+        sprintf(temp,"%d",result[i-1]%10);
+        resultStr[i-1] = temp[0];
+    }
+    char aa[1000];
+    if(improve){
+        sprintf(aa,"%d",improve);
+        strcat(aa,resultStr);
+        strcpy(resultStr,aa);
+    }
+    c_for_each(0,bit+1,resultStr,"%c");
+
+
     return s;
 }
 int main(){
@@ -25,6 +58,6 @@ int main(){
     //     printf("%d",n);
 
     // }
-    strcpy(s,"88.999");
-    multi(s,4);
+    strcpy(s,"331");
+    multi(s);
 }
