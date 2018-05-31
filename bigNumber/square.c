b380ab54f7e054b62af77cac2c99b4599371d93d
liuhuan <qq384012351@gmail.com>
2018-05-31 23:25:55 +0800

    finish the square of big number

--- bigNumber/square.c
+++ bigNumber/square.c
@@ -3,14 +3,11 @@
 #include "stdlib.h"
 
 #define BIT_MAX 1000
-#define c_for_each(begin, end, array,type) \
-                   for(int i=(begin); i<(end); ++i) printf(type,array[i]);
-char* multi(char* s){
-    int bit=0,pointBit=0,tempLen=0,result[BIT_MAX]={0},improve;
-    char resultStr[BIT_MAX];
+char* square(char* s){
+    int bit=0,pointBit=0,tempLen=0,result[BIT_MAX]={0},improve=0;
+    char resultStr[BIT_MAX],temp[4];
     int len =strlen(s);
     int splitNum[len];
-    char temp[1];
     for(int i=0;i<len;i++){
         splitNum[i]=0;
         if(s[i] =='.'){
@@ -26,37 +23,28 @@ char* multi(char* s){
            result[bit++] += splitNum[i-1]*splitNum[m-1];
         }
     }
-    improve = 0;
-    char tt[1];
-    for(int i=0;i<bit;i++){
+    for(int i=0;i<=bit;i++){
+        if(i==bit&&improve ==0 ){
+                bit--;
+        }
         result[i] += improve;
         improve = result[i]/10;
         sprintf(temp,"%d",result[i]%10);
-        tt[0] = temp[0];
-        resultStr[i] = tt[0];
-    }
-    if(improve){
-        sprintf(temp,"%d",improve);
-        resultStr[bit] = temp[0];
+        resultStr[i] = temp[0];
     }
     pointBit *=2;
     for(int i=bit;i>=0;i--){
         if(i == pointBit-1){
             putchar('.');
         }
-        printf("%c",resultStr[i]);
+        putchar(resultStr[i]);
     }
 
     return s;
 }
 int main(){
-    char s[7] ;
-    // int n;
-    // while(scanf("%s%d",s,&n)==2){
-    //     printf("%s",s);
-    //     printf("%d",n);
-
-    // }
-    strcpy(s,"33.1");
-    multi(s);
+    char s[10] ;
+    while(scanf("%s",s)==1){
+        square(s);
+    }
 }
